bd7cfc2b050bceebb65d9d21f6970cc8a48e4451
Marius Mandal <mariusmandal@gmail.com>
2013-10-14 14:24:37 +0200

    htaccess

--- index.php
+++ index.php
@@ -1,9 +1,11 @@
 <?php
-if(!isset($_GET['api'])) {
+var_dump($_GET);
+
+if(!isset($_GET['API']) || !isset($_GET['CALL'])) {
 	die(false);
 }
 
-if(!file_exists($_GET['api'].'.api.php'))
+if(!file_exists($_GET['API'].'/'.$_GET['CALL'].'.api.php'))
 	die(false);
 	
-require_once($_GET['api'].'.api.php');
+require_once($_GET['API'].'/'.$_GET['CALL'].'.api.php');
