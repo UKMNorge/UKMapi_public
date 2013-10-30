6c5c8e4fb14018336b2e47695982abc1b79b9077
Marius Mandal <mariusmandal@gmail.com>
2013-10-30 14:54:16 +0100

    debug - bug is here?

--- video/registrer.api.php
+++ video/registrer.api.php
@@ -87,7 +87,7 @@ if($innslag) {
 				array('cronid' => $cron_id));
 	
 	$res = $qry->run('array');
-
+	var_dump($res);
 	if($res) {
 		$data = video_calc_data('wp_related', $res);
 		tv_update($data);
