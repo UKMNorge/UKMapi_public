5fb0e8abae67f0ee7e3af10c0e8883079266de6c
Marius Mandal <mariusmandal@gmail.com>
2013-11-05 14:23:42 +0100

    restruct

--- video/tv_update.api.php
+++ video/tv_update.api.php
@@ -1,15 +1,39 @@
 <?php
 // http://api.ukm.no/video:updateTV/$id
+require_once('UKM/inc/tv/cron.functions.tv.php');
 
-if(isset($_POST['type']) && $_POST['type'] == 'standalone') {
-	$qry = new SQL("SELECT * 
-					FROM `ukm_standalone_video` 
-					WHERE `cron_id` = '#cron_id'",
+if(!isset($_POST['type'])) {
+	die('Missing type!');
+}
+
+switch($_POST['type']) {
+	case 'band_related':
+		die('Missing support for numeric ID');
+		/*
+		$qry = new SQL("SELECT * 
+					FROM `ukmno_wp_related`
+					WHERE `post_meta` LIKE '%#file%'
+					AND `post_type` = 'video'
+					ORDER BY `rel_id` DESC
+					LIMIT 1",
 					array('file' => $_GET['ID']));
-	$res = $qry->run('array');
-	if($res) {
-		require_once('UKM/inc/tv/cron.functions.tv.php');
-		$data = video_calc_data('standalone_video', $res );
-		tv_update($data);
-	}
+					
+		$res = $qry->run('array');
+		if($res) {
+			$data = video_calc_data('wp_related', $res);
+			tv_update($data);
+		}
+		*/
+		break;
+	case 'standalone':
+		$qry = new SQL("SELECT * 
+						FROM `ukm_standalone_video` 
+						WHERE `cron_id` = '#cron_id'",
+						array('file' => $_GET['ID']));
+		$res = $qry->run('array');
+		if($res) {
+			$data = video_calc_data('standalone_video', $res );
+			tv_update($data);
+		}
+		break;
 }
