7653f337619f931ea962fd8eeae8938e75c7c2a4
Marius Mandal <mariusmandal@gmail.com>
2015-01-12 11:33:52 +0100

    Last inn innslag uansett b_status (for å kunne korrigere statistikk)
    Innslag som er slettet får status != 8 før statistikken oppdateres, og vil derfor ikke bli lastet inn, og henger igjen i statistikk-tabellen

--- innslag/statistikk_oppdater.api.php
+++ innslag/statistikk_oppdater.api.php
@@ -3,6 +3,6 @@ if(!isset($_GET['ID']))
 	die(false);
 	
 require_once('UKM/innslag.class.php');
-$innslag = new innslag( $_GET['ID'] );
+$innslag = new innslag( $_GET['ID'], false );
 $innslag->statistikk_oppdater();
 die(json_encode(array('success' => true)));
