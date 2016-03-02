5ae2b619cfbd7f4228459bbef383f599dbc78615
Marius Mandal <mariusmandal@gmail.com>
2016-03-02 10:58:03 +0100

    Hent filinfo for playback

---
+++ playback/file.api.php
@@ -0,0 +1,18 @@
+<?php
+
+if( !isset( $_GET['file'] ) || !isset( $_GET['pl_id']) || empty( $_GET['file'] ) || empty( $_GET['pl_id'] ) ) {
+	die('Mangler identifikator'); 
+}
+
+require_once('UKM/sql.class.php');
+
+$sql = new SQL("SELECT * 
+				FROM `ukm_playback`
+				WHERE `pb_id` = '#id'
+				AND `pl_id` = '#plid'",
+			   array('id' => $_GET['file'], 'plid' => $_GET['pl_id'] )
+			  );
+$res = $sql->run();
+$file = mysql_fetch_assoc( $res );
+
+die( json_encode( $file ) );
