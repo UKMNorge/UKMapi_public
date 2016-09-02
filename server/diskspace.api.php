613f5bebfaca595f73ae44d967d30f4692501473
Marius Mandal <mariusmandal@gmail.com>
2016-09-02 11:47:27 +0200

    diskspace sjekker riktig mappe

--- server/diskspace.api.php
+++ server/diskspace.api.php
@@ -1,7 +1,12 @@
 <?php
-	
-$info = new stdClass;
-$info->diskspace = diskfreespace("/");
-$info->total_diskspace = disk_total_space("/");
+require_once('UKMconfig.inc.php');
 
+$info = new stdClass;
+if( 'ukm.no' == UKM_HOSTNAME ) {
+	$info->diskspace = diskfreespace("/home/");
+	$info->total_diskspace = disk_total_space("/home/" );
+} else {
+	$info->diskspace = diskfreespace("/");
+	$info->total_diskspace = disk_total_space("/" );
+}
 die(json_encode($info));
