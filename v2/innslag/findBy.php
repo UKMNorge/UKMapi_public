33a6dc63623b0d9cc9f149c572030ed75a08f5d6
Marius Mandal <mariusmandal@gmail.com>
2018-09-19 14:23:15 +0200

    Hent alternative bilde-størrelser

--- v2/innslag/findBy.php
+++ v2/innslag/findBy.php
@@ -7,7 +7,16 @@ $monstring = new monstring_v2( API_MONSTRING );
 switch( API_FINDBY_SELECTOR ) {
 	case 'id':
 		$innslag = $monstring->getInnslag()->get( API_FINDBY_ID );
-		$export = json_export::innslag( $innslag );
+        $export = json_export::innslag( $innslag );
+        $export->bilder = [];
+        
+        foreach( $innslag->getBilder()->getAll() as $bilde ) {
+            $export_bilde = json_export::bilde( $bilde );
+            $export_bilde->storrelser = [];
+            foreach( ['thumbnail','medium','large','lite'] as $storrelse ) {
+                $export_bilde->storrelser[ $storrelse ] = json_export::bilde( $bilde, $storrelse );
+            $export->bilder[] = $export_bilde;
+        }
         break;
 	default:
 		throw new Exception('Unknown findBy selector '. API_FINDBY_SELECTOR );
