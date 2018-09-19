6a94cc8b6d737bb29cdf41d56f5d958044ce9108
Marius Mandal <mariusmandal@gmail.com>
2018-09-19 13:47:45 +0200

    oi, masse unødig kode det

--- v2/monstringer/findBy.php
+++ v2/monstringer/findBy.php
@@ -6,19 +6,15 @@ require_once('UKM/monstringer.class.php');
 switch( API_FINDBY_SELECTOR ) {
 	case 'kommune':
 		$monstring = monstringer_v2::kommune( API_FINDBY_ID, API_SEASON );
-		
-		echo json_encode( json_export::monstring( $monstring ) );
-		die();
-
+		break;		
 	case 'fylke':
 		$monstring = monstringer_v2::fylke( API_FINDBY_ID, API_SEASON );
-		
-		echo json_encode( json_export::monstring( $monstring ) );
-		die();
+		break;
 	case 'id':
 		$monstring = new monstring_v2( API_FINDBY_ID );
-		echo json_encode( json_export::monstring( $monstring ) );
-		die();
+		break;
 	default:
 		throw new Exception('Unknown findBy selector '. API_FINDBY_SELECTOR );
 }
+echo json_encode( json_export::monstring( $monstring ) );
+die();
