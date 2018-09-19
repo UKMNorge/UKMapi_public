76c7fad433a535cb1ddce18d0d29ddda0c573f54
Marius Mandal <mariusmandal@gmail.com>
2018-09-19 13:54:40 +0200

    Mulig å finne innslag-info

---
+++ v2/innslag/findBy.php
@@ -0,0 +1,17 @@
+<?php
+
+require_once('UKM/monstringer.class.php');
+
+$monstring = new monstring_v2( API_MONSTRING );
+
+switch( API_FINDBY_SELECTOR ) {
+	case 'id':
+		$innslag = $monstring->getInnslag()->get( API_FINDBY_ID );
+		$export = json_export::innslag( $hendelse );
+        break;
+	default:
+		throw new Exception('Unknown findBy selector '. API_FINDBY_SELECTOR );
+}
+
+echo json_encode( $export );
+die();
