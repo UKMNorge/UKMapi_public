4a42cc85166637ee6f37fe3e4382449b1641a64e
Marius Mandal <mariusmandal@gmail.com>
2018-09-19 13:55:16 +0200

    vel, eh

--- v2/innslag/findBy.php
+++ v2/innslag/findBy.php
@@ -7,7 +7,7 @@ $monstring = new monstring_v2( API_MONSTRING );
 switch( API_FINDBY_SELECTOR ) {
 	case 'id':
 		$innslag = $monstring->getInnslag()->get( API_FINDBY_ID );
-		$export = json_export::innslag( $hendelse );
+		$export = json_export::innslag( $innslag );
         break;
 	default:
 		throw new Exception('Unknown findBy selector '. API_FINDBY_SELECTOR );
