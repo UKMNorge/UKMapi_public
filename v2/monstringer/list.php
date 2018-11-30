23b2383667a33e284f531054c30b627230719e47
Marius Mandal <mariusmandal@gmail.com>
2018-11-30 11:27:00 +0100

    List alle fylkesmønstringer

--- v2/monstringer/list.php
+++ v2/monstringer/list.php
@@ -1,3 +1,9 @@
 <?php
 
-die('ello');
+$fylker = [];
+$monstringer = monstringer_v2::getFylker( API_SEASON );
+foreach( $monstringer as $monstring ) {
+    $fylker[] = json_export::monstring( $monstring );
+}
+echo json_encode( $fylker );
+die();
