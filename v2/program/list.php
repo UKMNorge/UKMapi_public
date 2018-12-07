aff777590ef67b830f646dbc90e89e152d7219e5
Marius Mandal <mariusmandal@gmail.com>
2018-12-07 11:10:09 +0100

    hent hendelser uten dagsortering

---
+++ v2/program/list.php
@@ -0,0 +1,18 @@
+<?php
+
+require_once('UKM/monstring.class.php');
+
+if( !API_MONSTRING ) {
+	die('false');
+}
+
+$monstring = new monstring_v2( API_MONSTRING );
+$program = $monstring->getProgram()->getAllInkludertInterne();
+
+$hendelser = [];
+foreach( $program as $hendelse ) {
+    $hendelser[] = json_export::hendelse( $hendelse );
+}
+
+echo json_encode( $data );
+die();
