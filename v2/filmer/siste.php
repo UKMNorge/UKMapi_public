90ab21fc981743548b52f91f421ace238da5d174
Marius Mandal <mariusmandal@gmail.com>
2019-02-19 11:07:35 +0100

    tving limit

--- v2/filmer/siste.php
+++ v2/filmer/siste.php
@@ -2,12 +2,18 @@
 
 require_once('UKM/tv_files.class.php');
 
+define('LIMIT', 5);
 $files = new tv_files('place', API_MONSTRING);
-$files->limit( 5 );
+$files->limit( LIMIT );
 
 $filmer = [];
+$counter = 0;
 while( $tv = $files->fetch() ) {
+    $counter++;
     $filmer[] = json_export::tv( $tv );
+    if( $counter > LIMIT ) {
+        break;
+    }
 }
 
 echo json_encode( $filmer );
