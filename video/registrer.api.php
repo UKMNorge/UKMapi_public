756e2370f41a28c4f9a0cd74528942cf8ea53508
Marius Mandal <mariusmandal@gmail.com>
2021-06-13 17:20:48 +0200

    Forbedret tilbakemeldinger når registrering feiler

--- video/registrer.api.php
+++ video/registrer.api.php
@@ -33,30 +33,52 @@ require_once('UKM/vendor/autoload.php'); // fordi InnslagType bruker yaml 😱
 $arrangement = new Arrangement($_POST['pl_id']);
 
 // OPPDATER UPLOADED_VIDEO
-if ((int) $_POST['b_id'] > 0) {
-    $type = 'innslag';
-    $innslag = $arrangement->getInnslag()->get(intval($_POST['b_id']), true);
-    $film = Converted::registerInnslag(
-        intval($_POST['id']),                   // $cron_id
-        $arrangement,
-        $_POST['file_path'],
-        $_POST['file_name_store'],
-        $innslag
-    );
-} else {
-    $type = 'reportasje';
-    $film = Converted::registerReportasje(
-        intval($_POST['id']),   // $cron_id
-        $arrangement,
-        $_POST['file_path'],
-        $_POST['file_name_store']
+try {
+    if ((int) $_POST['b_id'] > 0) {
+        $type = 'innslag';
+        $innslag = $arrangement->getInnslag()->get(intval($_POST['b_id']), true);
+        $film = Converted::registerInnslag(
+            intval($_POST['id']),                   // $cron_id
+            $arrangement,
+            $_POST['file_path'],
+            $_POST['file_name_store'],
+            $innslag
+        );
+    } else {
+        $type = 'reportasje';
+        $film = Converted::registerReportasje(
+            intval($_POST['id']),   // $cron_id
+            $arrangement,
+            $_POST['file_path'],
+            $_POST['file_name_store']
+        );
+    }
+} catch( Exception $e ) {
+    error_log('CAUGHT REGISTER EXCEPTION (code'. $e->getCode() .':');
+    error_log($e->getMessage());
+    header('HTTP/1.1 500 INTERNAL SERVER ERROR');
+    die(
+        json_encode(
+            [
+                'success' => false,
+                'type' => 'unknown'
+            ]
+        )
     );
 }
 
+error_log('REGISTRER I UKM-TV');
 // FILMEN ER KLAR, REGISTRER I UKM-TV
-$tv_id = Converted::sendToUKMTV( $film );
+try {
+    $tv_id = Converted::sendToUKMTV( $film );
+} catch( Exception $e ) {
+    error_log('TV CAUGHT EXCEPTION (code'. $e->getCode() .':');
+    error_log($e->getMessage());
+    $tv_id = false;
+}
 
 // FEEDBACK TO VIDEOCONVERTER
+// SUCCESS
 if( !$tv_id ) {
     error_log('FAILURE REGISTRER:VIDEO CRON_ID: ' . var_export($_POST['id'], true));
     error_log('TYPE: '. strtoupper($type));
@@ -69,15 +91,16 @@ if( !$tv_id ) {
             ]
         )
     );
-} else {
-    error_log('SUCCESS REGISTRER:VIDEO CRON_ID: ' . var_export($_POST['id'], true));
-    error_log('TYPE: '. strtoupper($type));
-    die(
-        json_encode(
-            [
-                'success' => true,
-                'type' => $type
-            ]
-        )
-    );
 }
+
+// ERROR
+error_log('SUCCESS REGISTRER:VIDEO CRON_ID: ' . var_export($_POST['id'], true));
+error_log('TYPE: '. strtoupper($type));
+die(
+    json_encode(
+        [
+            'success' => true,
+            'type' => $type
+        ]
+    )
+);
