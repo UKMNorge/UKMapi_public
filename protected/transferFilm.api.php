ab24b3a38c3a6ddff9a329d43a366bca6c04be35
Kushtrim Aliu <kushtrimaliu19@gmail.com>
2025-05-15 17:01:03 +0200

    transferFilm på api

--- protected/transferFilm.api.php
+++ protected/transferFilm.api.php
@@ -3,11 +3,6 @@
 use UKMNorge\Filmer\UKMTV\CloudflareFilm;
 use UKMNorge\Filmer\UKMTV\Film;
 use UKMNorge\OAuth2\HandleAPICall;
-// use UKMNorge\Geografi\Kommune;
-// use UKMNorge\Arrangement\Arrangement;
-// use UKMNorge\Geografi\Fylker;
-// use UKMNorge\Nettverk\Omrade;
-
 
 require_once('UKM/Autoloader.php');
 
@@ -24,63 +19,19 @@ if (empty($cloudflare_id)) {
 
 $cloudflareFilm = null;
 
+
 try{ 
-    $cloudflareFilm = CloudflareFilm::getById($cloudflare_id);
+    $cloudflareFilm = new CloudflareFilm([], $cloudflare_id);
 } catch (Exception $e) {
     $handleCall->sendErrorToClient('Fant ikke film med id ' . $cloudflare_id, 404);
 }
 
+if (empty($cloudflareFilm)) {
+    $handleCall->sendErrorToClient('Fant ikke film med id ' . $cloudflare_id, 404);
+}
 
 $convertedFilm = Film::convertFromCloudflare($cloudflareFilm);
 
 $handleCall->sendToClient([
     'status' => $convertedFilm->getId() == $cloudflareFilm->getId(),
 ]);
-
-// $kommuneNavn = $handleCall->getArgument('kommune');
-// $kommuneId = $handleCall->getArgument('kommunenummer');
-
-
-// $tilgjengelige_arrangementer = [];
-// try {
-//     $kommune = new Kommune($kommuneId);
-    
-
-//     // If Oslo, returner alle arrangementer i alle bydeler
-//     if( $kommuneId == '0301') {
-//         $osloFylke = Fylker::getById(3);
-        
-//         foreach( $osloFylke->getKommuner()->getAll() as $kommune ) {
-//             $alle_arrangementer = Omrade::getByKommune($kommune->getId())->getKommendeArrangementer()->getAll();
-//             foreach( $alle_arrangementer as $arrangement ) {
-//                 $tilgjengelige_arrangementer[] = _generateArrangement($arrangement);
-//             }
-//         }
-//     }
-//     else {
-//         $alle_arrangementer = Omrade::getByKommune($kommune->getId())->getKommendeArrangementer()->getAll();
-//     }
-    
-//     foreach ($alle_arrangementer as $arrangement) {
-//         $tilgjengelige_arrangementer[] = _generateArrangement($arrangement);
-//     }
-
-//     $handleCall->sendToClient([
-//         'status' => true,
-//         'arrangementer' => $tilgjengelige_arrangementer,
-//     ]);
-
-// } catch( Exception $e ) {
-//     $handleCall->sendErrorToClient('Det har oppstått en serverfeil', 500);
-// }
-
-
-// // Private funksjoner
-// function _generateArrangement(Arrangement $arrangement) {
-//     return [
-//         'id' => $arrangement->getId(),
-//         'navn' => $arrangement->getNavn(),
-//         'url' => $arrangement->getLink(),
-//         'dato' => $arrangement->getStart()
-//     ];
-// }
