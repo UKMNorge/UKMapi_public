369cdd1d6154c6a241526a421a8d5ca08f449026
Mathias Novas <mathias.novas@try.no>
2025-08-06 10:47:15 +0200

    Add end point for getting all municipalities

---
+++ nettside/alle_kommuner.api.php
@@ -0,0 +1,26 @@
+<?php
+
+use UKMNorge\OAuth2\HandleAPICall;
+use UKMNorge\Geografi\Kommuner;
+
+require_once('UKM/Autoloader.php');
+
+header('Content-Type: application/json; charset=utf-8');
+header('Access-Control-Allow-Origin: *');
+
+$handleCall = new HandleAPICall([], [], ['GET', 'POST'], false);
+
+$retKommuner = [];
+try{
+    $kommuner = Kommuner::getAll();
+    foreach($kommuner as $kommune) {
+        $retKommuner[] = $kommune;
+    }
+} catch( Exception $e ) {
+    $handleCall->sendErrorToClient('Det har oppstått en serverfeil', 500);
+    return;
+}
+
+$handleCall->sendToClient(
+    $retKommuner
+);
