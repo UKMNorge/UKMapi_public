fefd8937b02ef962fc91ed57d87919c75e9d15d0
Kushtrim Aliu <kushtrimaliu19@gmail.com>
2025-08-08 16:04:59 +0200

    alle kontaktpersoner på fylke og kommune nivå

---
+++ nettside/alle_kontaktpersoner.api.php
@@ -0,0 +1,63 @@
+<?php
+
+use UKMNorge\Geografi\Fylker;
+use UKMNorge\OAuth2\HandleAPICall;
+use UKMNorge\Tools\ObjectTransformer;
+use UKMNorge\Nettverk\OmradeKontaktpersoner;
+
+
+require_once('UKM/Autoloader.php');
+
+header('Content-Type: application/json; charset=utf-8');
+header('Access-Control-Allow-Origin: *');
+
+$handleCall = new HandleAPICall([], [], ['GET', 'POST'], false);
+
+
+$retOmradeKontakpersoner = [];
+
+// For hvert fylke, legg til kontaktpersoner
+foreach(Fylker::getAll() as $fylke) {
+    $omradeId = $fylke->getId();
+    $omradeType = 'fylke';
+    
+    $oKFylke = new OmradeKontaktpersoner($omradeId, $omradeType);
+    
+    if(!isset($retOmradeKontakpersoner[$omradeType. '_' .$omradeId])) {
+        $retOmradeKontakpersoner[$omradeType. '_' .$omradeId] = [
+            'omrade_id' => $omradeId,
+            'omrade_type' => $omradeType,
+            'omrade_navn' => $fylke->getNavn(),
+            'kontaktpersoner' => []
+        ];
+    }
+
+    foreach($oKFylke->getAll() as $kontaktperson) {
+        $retOmradeKontakpersoner[$omradeType. '_' .$omradeId]['kontaktpersoner'][] = ObjectTransformer::kontaktperson($kontaktperson);
+    }
+
+    // For hver kommune i fylket, legg til kontaktpersoner
+    foreach($fylke->getKommuner()->getAll() as $kommune) {
+        $kommuneId = $kommune->getId();
+        $kommuneType = 'kommune';
+        
+        if(!isset($retOmradeKontakpersoner[$kommuneType. '_' .$kommuneId])) {
+            $retOmradeKontakpersoner[$kommuneType. '_' .$kommuneId] = [
+                'omrade_id' => $kommuneId,
+                'omrade_type' => $kommuneType,
+                'omrade_navn' => $kommune->getNavn(),
+                'kontaktpersoner' => []
+            ];
+        }
+        
+        $oKKommune = new OmradeKontaktpersoner($kommuneId, $kommuneType);
+        
+        foreach($oKKommune->getAll() as $kontaktperson) {
+            $retOmradeKontakpersoner[$kommuneType. '_' .$kommuneId]['kontaktpersoner'][] = ObjectTransformer::kontaktperson($kontaktperson);
+        }
+    }
+}
+
+$handleCall->sendToClient(
+    $retOmradeKontakpersoner
+);
