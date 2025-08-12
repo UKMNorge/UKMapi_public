9ca4b20f12a8b7092ef9839f016abc8ec0a3358b
Kushtrim Aliu <kushtrimaliu19@gmail.com>
2025-08-13 00:00:28 +0200

    Integrering av kontaktpersoner fra admins

--- nettside/alle_kontaktpersoner.api.php
+++ nettside/alle_kontaktpersoner.api.php
@@ -1,18 +1,35 @@
 <?php
+// Show all errors, warnings, and notices
+// error_reporting(E_ALL); 
+// ini_set('display_errors', 1);
+// ini_set('display_startup_errors', 1);
+
+// Optional: Force errors to be shown even in production environments
+// ini_set('log_errors', 0);
 
 use UKMNorge\Geografi\Fylker;
+use UKMNorge\Nettverk\Omrade;
 use UKMNorge\OAuth2\HandleAPICall;
 use UKMNorge\Tools\ObjectTransformer;
 use UKMNorge\Nettverk\OmradeKontaktpersoner;
+use UKMNorge\Database\SQL\Query;
+use UKMNorge\Nettverk\Administratorer;
 
 
+// require_once '/full/path/to/wp-blog-header.php';
+
+// dev-parellels/dev-html
+
+
+// require_once( dirname(dirname(dirname(dirname( __FILE__ )))) . '/wp-blog-header.php' );
+
+// die;
 require_once('UKM/Autoloader.php');
 
 header('Content-Type: application/json; charset=utf-8');
 header('Access-Control-Allow-Origin: *');
 
 $handleCall = new HandleAPICall([], [], ['GET', 'POST'], false);
-
 
 $retOmradeKontakpersoner = [];
 
@@ -22,7 +39,8 @@ foreach(Fylker::getAll() as $fylke) {
     $omradeType = 'fylke';
     
     $oKFylke = new OmradeKontaktpersoner($omradeId, $omradeType);
-    
+    $omradeFylke = new Omrade($omradeType, $omradeId);    
+
     if(!isset($retOmradeKontakpersoner[$omradeType. '_' .$omradeId])) {
         $retOmradeKontakpersoner[$omradeType. '_' .$omradeId] = [
             'omrade_id' => $omradeId,
@@ -31,10 +49,26 @@ foreach(Fylker::getAll() as $fylke) {
             'kontaktpersoner' => []
         ];
     }
+    
 
     foreach($oKFylke->getAll() as $kontaktperson) {
         $retOmradeKontakpersoner[$omradeType. '_' .$omradeId]['kontaktpersoner'][] = ObjectTransformer::kontaktperson($kontaktperson);
     }
+
+
+    // Get alle administratorer som er kontaktpersoner for fylket
+    $adminer = new Administratorer($omradeType, $omradeId);
+    foreach($adminer->getAll() as $kontaktpersonAdmin) {
+        if($kontaktpersonAdmin->erKontaktperson($omradeFylke) != true) {
+            continue; // Hopper over hvis admin ikke er kontaktperson for fylket
+        }
+
+        $admin = getAdminInfoFromWP($kontaktpersonAdmin->getId());
+        if($admin != null) {
+            $retOmradeKontakpersoner[$omradeType. '_' .$omradeId]['kontaktpersoner'][] = ObjectTransformer::adminKontaktperson($admin);
+        }
+    }
+
 
     // For hver kommune i fylket, legg til kontaktpersoner
     foreach($fylke->getKommuner()->getAll() as $kommune) {
@@ -53,9 +87,24 @@ foreach(Fylker::getAll() as $fylke) {
         }
         
         $oKKommune = new OmradeKontaktpersoner($kommuneId, $kommuneType);
+        $omradeKommune = new Omrade($kommuneType, $kommuneId);    
+
         
         foreach($oKKommune->getAll() as $kontaktperson) {
             $retOmradeKontakpersoner[$kommuneType. '_' .$kommuneId]['kontaktpersoner'][] = ObjectTransformer::kontaktperson($kontaktperson);
+        }
+
+        // Get alle administratorer som er kontaktpersoner for kommunen
+        $adminer = new Administratorer($kommuneType, $kommuneId);
+        foreach($adminer->getAll() as $kontaktpersonAdmin) {
+            if($kontaktpersonAdmin->erKontaktperson($omradeKommune) != true) {
+                continue; // Hopper over hvis admin ikke er kontaktperson for kommunen
+            }
+            $admin = getAdminInfoFromWP($kontaktpersonAdmin->getId());
+            if($admin != null) {
+                // Add admin contact person to the kommune
+                $retOmradeKontakpersoner[$kommuneType. '_' .$kommuneId]['kontaktpersoner'][] = ObjectTransformer::adminKontaktperson($admin);
+            }
         }
     }
 }
@@ -63,3 +112,25 @@ foreach(Fylker::getAll() as $fylke) {
 $handleCall->sendToClient(
     $retOmradeKontakpersoner
 );
+
+
+function getAdminInfoFromWP($adminId) {
+    $query = new Query(
+        "SELECT
+            u.display_name,
+            um.meta_value AS user_phone
+        FROM wpms2012_users AS u
+        LEFT JOIN wpms2012_usermeta AS um
+        ON um.user_id = u.ID
+        AND um.meta_key = 'user_phone'
+        WHERE u.ID = '#user_id'",
+        [
+            'user_id' => $adminId
+        ], 
+        'wordpress'
+    );
+    
+    
+    $query->setDatabase('wordpress');
+    return $query->run('array');
+}
