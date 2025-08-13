61f5dde344ecae7353f283cde34de198860d0045
Kushtrim Aliu <kushtrimaliu19@gmail.com>
2025-08-13 14:03:06 +0200

    Filtering av arrangementer på type
    Filtrering av arrangementer gjennom valgfritt argument 'type'

--- nettside/alle_arrangementer.api.php
+++ nettside/alle_arrangementer.api.php
@@ -9,14 +9,21 @@ require_once('UKM/Autoloader.php');
 header('Content-Type: application/json; charset=utf-8');
 header('Access-Control-Allow-Origin: *');
 
-$handleCall = new HandleAPICall(['season'], [], ['GET', 'POST'], false);
+$handleCall = new HandleAPICall(['season'], ['type'], ['GET', 'POST'], false);
 
 $seasonArg = $handleCall->getArgument('season');
+$typeArg = $handleCall->getOptionalArgument('type') ?? null;
 
 if (!is_numeric($seasonArg)) {
-    $handleCall->sendErrorToClient('Sesong må være int', 400);
+    $handleCall->sendErrorToClient('Sesong må være tall', 400);
     return;
 }
+
+if($typeArg != null && !in_array($typeArg, ['kommune', 'fylke', 'land'])) {
+    $handleCall->sendErrorToClient('Ugyldig type', 400);
+    return;
+}
+
 $season = intval($seasonArg);
 
 if( $season < 2020 ) {
@@ -27,6 +34,11 @@ if( $season < 2020 ) {
 $retArrangementer = [];
 try{ 
     $arrangementer = Load::bySesong($season)->getAll();
+    if( $typeArg ) {
+        $arrangementer = array_filter($arrangementer, function($arrangement) use ($typeArg) {
+            return $arrangement->getType() == $typeArg;
+        });
+    }
     foreach($arrangementer as $arrangement) {
         $retArrangementer[] = ObjectTransformer::arrangement($arrangement);
     }
