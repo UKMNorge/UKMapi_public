6445b5d1f3817cffe6857cf11f0f85a8a80422aa
Kushtrim Aliu <kushtrimaliu19@gmail.com>
2025-11-30 13:46:39 +0100

    UKM Norge nasjonalt nivå variabel til kontaktperson()

--- nettside/alle_kontaktpersoner.api.php
+++ nettside/alle_kontaktpersoner.api.php
@@ -41,7 +41,8 @@ try {
         
     
         foreach($oKFylke->getAll() as $kontaktperson) {
-            $retOmradeKontakpersoner[$omradeType. '_' .$omradeId]['kontaktpersoner'][] = ObjectTransformer::kontaktperson($kontaktperson);
+            $erUKMNorge = $fylke->getId() == 9999 ? true : false;
+            $retOmradeKontakpersoner[$omradeType. '_' .$omradeId]['kontaktpersoner'][] = ObjectTransformer::kontaktperson($kontaktperson, $erUKMNorge);
         }
     
     
