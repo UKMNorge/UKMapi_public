18f2fbf3a9202c9230f0b9a3f326bc214f052a43
Kushtrim Aliu <kushtrimaliu19@gmail.com>
2026-05-28 17:57:26 +0200

    Revert "Svar kommer nå."
    This reverts commit 8ae03008f7cef9ef21404777c13cf23e465f285d.

--- protected/getAlleDeltakereSvar.api.php
+++ protected/getAlleDeltakereSvar.api.php
@@ -7,27 +7,23 @@ use UKMNorge\Arrangement\UKMFestival;
 use UKMNorge\Arrangement\Skjema\Skjema;
 use UKMNorge\Arrangement\Skjema\SvarSett;
 
+
+
 require_once('UKM/Autoloader.php');
 
 header('Content-Type: application/json; charset=utf-8');
 header('Access-Control-Allow-Origin: *');
 
-$handleCall = new HandleAPICall(
-    ['oppgave_id', 'sporsmal_id', 'skjema_id'],
-    [],
-    ['GET', 'POST'],
-    false,
-    false,
-    true
-);
+$handleCall = new HandleAPICall(['oppgave_id', 'sporsmal_id'], ['skjema_id'], ['GET', 'POST'], false, false, true);
 
-try {
+$arrangement = null;
+try{
     $arrangement = UKMFestival::getCurrentUKMFestival();
-    if (!$arrangement) {
+    if(!$arrangement) {
         $handleCall->sendErrorToClient('Kunne ikke hente arrangementet', 401);
     }
-} catch (Exception $e) {
-    if ($e->getCode() == 401) {
+} catch(Exception $e) {
+    if($e->getCode() == 401) {
         $handleCall->sendErrorToClient($e->getMessage(), 401);
     }
     $handleCall->sendErrorToClient('Kunne ikke hente arrangementet', 401);
@@ -37,34 +33,28 @@ $oppgaveId = (int) $handleCall->getArgument('oppgave_id');
 $skjemaId = (int) $handleCall->getArgument('skjema_id');
 $sporsmalId = (int) $handleCall->getArgument('sporsmal_id');
 
-if ($oppgaveId < 1) {
-    $handleCall->sendErrorToClient('Ugyldig oppgave_id', 400);
-}
-if ($skjemaId < 1) {
-    $handleCall->sendErrorToClient('Ugyldig skjema_id', 400);
-}
-if ($sporsmalId < 1) {
+if($sporsmalId == null || $sporsmalId < 1) {
     $handleCall->sendErrorToClient('Ugyldig sporsmal_id', 400);
 }
-
+$oppgave = null;
+if($oppgaveId < 1) {
+    $handleCall->sendErrorToClient('Ugyldig oppgave_id', 400);
+}
 try {
     $oppgave = new Oppgave($oppgaveId);
-} catch (Exception $e) {
+} catch(Exception $e) {
     $handleCall->sendErrorToClient('Fant ikke oppgaven', 404);
 }
 
-$valgtSkjema = null;
-foreach ($oppgave->getSkjemaKjede() as $skjemaItem) {
-    $kjedelinkId = (int) $skjemaItem->getId();
-    $faktiskSkjemaId = (int) $skjemaItem->getSkjema()->getId();
-
-    if ($kjedelinkId === $skjemaId || $faktiskSkjemaId === $skjemaId) {
-        $valgtSkjema = $skjemaItem;
+$skjema = null;
+foreach($oppgave->getSkjemaKjede() as $skjema) {
+    if($skjema->getId() === $skjemaId) {
+        $skjema = $skjema;
         break;
     }
 }
 
-if ($valgtSkjema === null) {
+if($skjema === null) {
     $handleCall->sendErrorToClient('Fant ikke skjemaet', 404);
 }
 
@@ -72,24 +62,18 @@ $retArr = [];
 
 foreach (VideresendingNominasjon::getAlleTilArrangement($arrangement->getId())->getAll() as $vNominasjon) {
     $person = $vNominasjon->getPerson();
-    if (!$person) {
+    if(!$person) {
         continue;
     }
+
+    $svarsett = getSvarSett($skjema->getSkjema(), $person->getId());
+    $svarText = $svarsett->get($sporsmalId)->getValue();
 
     $arrangementFra = $vNominasjon->getArrangementFra();
     $innslag = $arrangementFra->getInnslag()->get($vNominasjon->getBId());
-    if (!$innslag) {
+    
+    if(!$innslag) {
         continue;
-    }
-
-    $svarsett = getSvarSett($valgtSkjema->getSkjema(), $person->getId());
-
-    $svarText = null;
-    try {
-        $svarObj = $svarsett->get($sporsmalId);
-        $svarText = $svarObj ? $svarObj->getValue() : null;
-    } catch (Exception $e) {
-        $svarText = null;
     }
 
     $retArr[$vNominasjon->getPId()] = [
@@ -104,8 +88,8 @@ foreach (VideresendingNominasjon::getAlleTilArrangement($arrangement->getId())->
     ];
 }
 
-function getSvarSett(Skjema $skjema, int $personId): SvarSett
-{
+
+function getSvarSett(Skjema $skjema, int $personId): SvarSett {
     try {
         $respondent = $skjema->getRespondenter()->get($personId);
         return $respondent->getSvar();
