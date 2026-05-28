25f8f1ff39fae5e3b22df8c3b89b47a098eb0a5b
Jardar Nordbo <jardar@ukm.no>
2026-05-28 19:01:01 +0000

    fungerende

--- protected/getAlleDeltakereSvar.api.php
+++ protected/getAlleDeltakereSvar.api.php
@@ -6,7 +6,7 @@ use UKMNorge\Videresending\VideresendingNominasjon;
 use UKMNorge\Arrangement\UKMFestival;
 use UKMNorge\Arrangement\Skjema\Skjema;
 use UKMNorge\Arrangement\Skjema\SvarSett;
-
+use UKMNorge\Database\SQL\Query;
 
 
 require_once('UKM/Autoloader.php');
@@ -30,8 +30,10 @@ try{
 }
 
 $oppgaveId = (int) $handleCall->getArgument('oppgave_id');
-$skjemaId = (int) $handleCall->getArgument('skjema_id');
+$skjemaId = (int) $handleCall->getOptionalArgument('skjema_id');
 $sporsmalId = (int) $handleCall->getArgument('sporsmal_id');
+
+var_dump($oppgaveId, $skjemaId, $sporsmalId);
 
 if($sporsmalId == null || $sporsmalId < 1) {
     $handleCall->sendErrorToClient('Ugyldig sporsmal_id', 400);
@@ -47,9 +49,9 @@ try {
 }
 
 $skjema = null;
-foreach($oppgave->getSkjemaKjede() as $skjema) {
-    if($skjema->getId() === $skjemaId) {
-        $skjema = $skjema;
+foreach($oppgave->getSkjemaKjede() as $s) {
+    if($s->getSkjema()->getId() == $skjemaId) {
+        $skjema = $s->getSkjema();
         break;
     }
 }
@@ -58,40 +60,76 @@ if($skjema === null) {
     $handleCall->sendErrorToClient('Fant ikke skjemaet', 404);
 }
 
+$alleRespondenter = $skjema->getRespondenter()->getAll();
 $retArr = [];
 
+
 foreach (VideresendingNominasjon::getAlleTilArrangement($arrangement->getId())->getAll() as $vNominasjon) {
-    $person = $vNominasjon->getPerson();
-    if(!$person) {
+    $personNominasjon = $vNominasjon->getPerson();
+    if(!$personNominasjon) {
         continue;
     }
-
-    $svarsett = getSvarSett($skjema->getSkjema(), $person->getId());
-    $svarText = $svarsett->get($sporsmalId)->getValue();
-
-    $arrangementFra = $vNominasjon->getArrangementFra();
-    $innslag = $arrangementFra->getInnslag()->get($vNominasjon->getBId());
     
-    if(!$innslag) {
-        continue;
-    }
+    $personIds = getPersonIdsByMobil($personNominasjon->getMobil());
+    foreach($personIds as $personId) {
+        try {
+            $svarsett = getSvarSett($skjema, $personId, $alleRespondenter);
+            $svarText = $svarsett->get($sporsmalId)->getValue();
+        } catch (Exception $e) {
+            $svarText = 'fant ikke svarsett';
+        }
+        
+        $arrangementFra = $vNominasjon->getArrangementFra();
+        try {
+            $innslag = $arrangementFra->getInnslag()->get($vNominasjon->getBId());
+        } catch(Exception $e) {
+            $innslag = null;
+        }
+    
+        if($svarText == null && isset($retArr[$vNominasjon->getPId()])) {
+            continue;
+        }
 
-    $retArr[$vNominasjon->getPId()] = [
-        'person_id' => $person->getId(),
-        'fornavn' => $person->getFornavn(),
-        'etternavn' => $person->getEtternavn(),
-        'mobil' => $person->getMobil(),
-        'fylke' => $arrangementFra->getFylke()->getNavn(),
-        'innslag' => $innslag->getNavn(),
-        'innslag_type' => $innslag->getType()->getNavn(),
-        'svar' => $svarText,
-    ];
+        $retArr[$vNominasjon->getPId()] = [
+            'person_id' => $personNominasjon->getId(),
+            'fornavn' => $personNominasjon->getFornavn(),
+            'etternavn' => $personNominasjon->getEtternavn(),
+            'mobil' => $personNominasjon->getMobil(),
+            'fylke' => $arrangementFra->getFylke()->getNavn(),
+            'innslag' => $innslag ? $innslag->getNavn() : null,
+            'innslag_type' => $innslag ? $innslag->getType()->getNavn() : null,
+            'svar' => $svarText,
+        ];
+    }
 }
 
+function getPersonIdsByMobil(string $mobil): array {
+   
+    $sql = new Query(
+        "SELECT p_id FROM `smartukm_participant`
+                    WHERE `p_phone` = '#mobil'",
+        ['mobil' => $mobil],
+    );
+    $res = $sql->run();
+    $personIds = [];
+    while ($row = Query::fetch($res)) {
+        $personIds[] = $row['p_id'];
+    }
+    return $personIds;
+}
 
-function getSvarSett(Skjema $skjema, int $personId): SvarSett {
+function getSvarSett(Skjema $skjema, int $personId, array &$alleRespondenter) {
     try {
-        $respondent = $skjema->getRespondenter()->get($personId);
+        $respondent = null;
+        foreach($alleRespondenter as $r) {
+            if($r->getId() == $personId) {
+                $respondent = $r;
+                break;
+            }
+        }
+        if(!$respondent) {
+            return SvarSett::getPlaceholder('person', $personId, $skjema->getId());
+        }
         return $respondent->getSvar();
     } catch (Exception $e) {
         if ($e->getCode() == 163003) {
